51550e61ae804e49c3897c017322a891d310b947
fernandapuc2025 <fernandapuc2025@gmail.com>
2026-02-19 17:07:29 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
-export const metadata = {
-  title: "Sistema SLA",
-  description: "Sistema de Lições Aprendidas",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <nav>
+          <a href="/">Dashboard</a> | <a href="/new-lesson">Nova Lição</a>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
