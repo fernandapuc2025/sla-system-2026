a48ad2cde80e41c9cecf0d2e75d1cdef21b667ae
fernandapuc2025 <fernandapuc2025@gmail.com>
2026-02-24 18:39:06 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { createClient } from '@supabase/supabase-client';
+import { createClient } from '@supabase/supabase-js';
 import { ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine, LabelList } from 'recharts';
 
 // Configuração do Supabase
